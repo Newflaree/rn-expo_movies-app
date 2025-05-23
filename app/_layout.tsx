6c884cf53e717f6e69ffd694870bd453d7b9bd2f
Camilo López <camiloj.lopez79@gmail.com>
2025-05-23 00:43:46 -0400

    Inicio de proyecto - MoviesApp

---
+++ app/_layout.tsx
@@ -0,0 +1,17 @@
+// React Native
+import { 
+  View,
+  Text
+} from 'react-native';
+import '../global.css';
+
+
+const RootLayout = () => {
+  return (
+    <View>
+      <Text className='text-3xl text-blue-500'>RootLayout</Text>
+    </View>
+  );
+}
+
+export default RootLayout;
