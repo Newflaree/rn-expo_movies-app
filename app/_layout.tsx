040f9cbde32e3d3c14984b1734280fc55098b49c
Camilo López <camiloj.lopez79@gmail.com>
2025-05-26 02:26:45 -0400

    Cliente de Axios - Primera patición

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,9 +4,12 @@ import {
   Text
 } from 'react-native';
 import '../global.css';
+import { nowPlayingAction } from '@/core/actions/movies/now-playing.action';
 
 
 const RootLayout = () => {
+  nowPlayingAction();
+
   return (
     <View>
       <Text className='text-3xl text-blue-500'>RootLayout</Text>
