786da73fad9e1faeb6cffd49fe86f66dbaf88977
Camilo López <camiloj.lopez79@gmail.com>
2025-05-30 07:56:05 -0400

    Mostrar detalles de la película

--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -7,13 +7,14 @@ import {
 } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
 import { useMovie } from '@/presentation/hooks/useMovie';
+import MovieHeader from '@/presentation/components/movie/MovieHeader';
 
 
 const MovieScreen = () => {
   const { id } = useLocalSearchParams();
   const { movieQuery } = useMovie( +id );
 
-  if ( movieQuery.isLoading ) {
+  if ( movieQuery.isLoading || !movieQuery.data ) {
     return (
       <View className='flex flex-1 justify-center items-center'>
         <Text className='mb-4'>Espere, por favor</Text>
@@ -24,7 +25,11 @@ const MovieScreen = () => {
 
   return (
     <ScrollView>
-      <Text>{ movieQuery.data?.title ?? 'No tiene título' }</Text>
+      <MovieHeader
+        poster={ movieQuery.data.poster }
+        originalTitle={ movieQuery.data.originalTitle }
+        title={ movieQuery.data.title }
+      />
     </ScrollView>
   );
 }
